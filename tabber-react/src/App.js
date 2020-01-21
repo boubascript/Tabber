e932dc25ac49e76aca44faa3c400c82fea5be3c7
Boubacar <boubacar.diallo1@macaulay.cuny.edu>
2020-01-20 23:33:07 -0500

    installed material-ui for ui framework

--- tabber-react/src/App.js
+++ tabber-react/src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tabber React soon come
+          Tabber React soon come !!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="contained" color="primary">
+          Save Tab
+        </Button>
       </header>
     </div>
   );
 }
-
-export default App;
